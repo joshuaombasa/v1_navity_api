c990c8fbf73828a2d838865ad0aee762a9f69fc2
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-18 18:58:59 +0300

    initial commit

---
+++ controllers/vans.js
@@ -0,0 +1,80 @@
+const Van = require('../models/van')
+const vansRouter = require('express').Router()
+
+
+vansRouter.get('/', async (request, response, next) => {
+    try {
+        const vans = await Van.find({})
+        response.json(vans)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+vansRouter.get('/:id', async (request, response, next) => {
+    try {
+        const van = await Van.findById(request.params.id)
+        if (!van) {
+            return response.status(404).end()
+        }
+        response.json(van)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+vansRouter.post('/', async (request, response, next) => {
+    const { name, price, description, imageUrl, type } = request.body
+
+    const vanData = new Van({
+        name,
+        price,
+        description,
+        imageUrl,
+        type
+    })
+    try {
+        const savedVan = await vanData.save()
+        response.status(201).json(savedVan)
+    } catch (error) {
+        next(error)
+    }
+})
+
+vansRouter.put('/:id', async (request, response, next) => {
+    const { name, price, description, imageUrl, type } = request.body
+    try {
+
+        const van = await Van.findById(request.params.id)
+        if (!van) {
+            return response.status(404).end()
+        }
+        const vanData = {
+            name,
+            price,
+            description,
+            imageUrl,
+            type
+        }
+        const updatedVan = await Van.findByIdAndUpdate(request.params.id,
+            vanData,
+            { new: true })
+        response.status(201).json(updatedVan)
+    } catch (error) {
+        console.log(error.name)
+        next(error)
+    }
+})
+
+vansRouter.delete('/:id', async (request, response, next) => {
+    try {
+        await Van.findByIdAndDelete(request.params.id)
+        response.sendStatus(204)
+    } catch (error) {
+        next(error)
+    }
+})
+
+module.exports = vansRouter
