c0bd30693c72bb1911e937ed412d174e1bf51136
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-22 21:34:04 +0300

    initial commit

--- models/van.js
+++ models/van.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose')
 
 const vanSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    type: {type: String, required: true},
-    price: {type: Number, required: true},
-    description: {type: String, required: true},
-    imageUrl: {type: String, required: true},
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId, ref
+            : 'User'
+    }
 })
 
 vanSchema.set('toJSON', {
-    transform: (document,returnedObject) => {
+    transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
